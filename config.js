9acaba3f328e73867d9a5bb2a06b25c27f3a648f
servicek38-blip <servicek38@gmail.com>
2026-05-11 19:09:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://foqihawejbzjbqknlyjj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_D7fhUpgvzAQW-t2TyKTbpA_nDzLJCK8";
+const MASTER_ADMIN_EMAIL = "servicek38@gmail.com";
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "service-k38-v4";
+const LOCAL_ASSETS = [
+  "./",
+  "./index.html",
+  "./config.js",
+  "./logo-k38.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./manifest.webmanifest",
+  "./sound-click.wav",
+  "./sound-success.wav",
+  "./sound-error.wav"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(LOCAL_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") return caches.match("./index.html");
+      });
+    })
+  );
+});
